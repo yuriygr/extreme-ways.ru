92a0c42372d8889b853ed04c3773bc61053b3839
Yuriy Grinev <a1d516ac5f5d290@gmail.com>
2026-05-21 11:10:27 +0300

    fix: meta, empty slogan on start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import vue from "@astrojs/vue";
-import partytown from '@astrojs/partytown'
 
 export default defineConfig({
   site: 'https://extreme-ways.ru',
   integrations: [
-    vue(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      }
-    })
+    vue()
   ],
   devToolbar: {
     enabled: false
